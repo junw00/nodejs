00ee43727c1a36a980ff2603a803dd466eb32749
junw00 <ljw143435@gmail.com>
2024-05-18 01:29:55 +0900

    node 1일차

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+
+app.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/news', (req, res) => {
+    res.send('오늘 비옴1')
+})
